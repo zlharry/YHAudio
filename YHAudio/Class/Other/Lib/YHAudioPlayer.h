f66bb72ad6b42ca8966da799a2a1ef94ea787ec6
harry <yuanhua_zhong@icloud.com>
2017-06-07 17:09:19 +0800

    添加了播放声音的工具类。

---
+++ YHAudio/Class/Other/Lib/YHAudioPlayer.h
@@ -0,0 +1,13 @@
+//
+//  YHAudioPlayer.h
+//  YHAudio
+//
+//  Created by harry on 2017/6/7.
+//  Copyright © 2017年 harry. All rights reserved.
+//  一个播放声音的工具
+
+#import <Foundation/Foundation.h>
+
+@interface YHAudioPlayer : NSObject
+
+@end
---
+++ YHAudio/Class/Other/Lib/YHAudioPlayer.m
@@ -0,0 +1,13 @@
+//
+//  YHAudioPlayer.m
+//  YHAudio
+//
+//  Created by harry on 2017/6/7.
+//  Copyright © 2017年 harry. All rights reserved.
+//
+
+#import "YHAudioPlayer.h"
+
+@implementation YHAudioPlayer
+
+@end
