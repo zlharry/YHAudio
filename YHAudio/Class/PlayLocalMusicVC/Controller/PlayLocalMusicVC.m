99f0255095e9e2dd5714b4a9e2a736d998f2709b
harry <yuanhua_zhong@icloud.com>
2017-06-13 14:49:51 +0800

    写了一个简单的音乐播放器界面。

--- YHAudio/Class/PlayLocalMusicVC/Controller/PlayLocalMusicVC.m
+++ YHAudio/Class/PlayLocalMusicVC/Controller/PlayLocalMusicVC.m
@@ -12,6 +12,19 @@
 
 @interface PlayLocalMusicVC ()
 @property (nonatomic, strong) YHMusicPlayer *musicPlayer;
+
+/** 播放／暂停按钮 */
+@property (nonatomic, weak) UIButton *playPauseBtn;
+
+/** 停止按钮 */
+@property (nonatomic, weak) UIButton *stopBtn;
+
+/** 进度条 */
+@property (nonatomic, weak) UISlider *slider;
+
+/** 播放器控制条背景 */
+@property (nonatomic, weak) UIView *controlBackView;
+
 @end
 
 @implementation PlayLocalMusicVC
@@ -21,40 +34,130 @@
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = @"本地音乐播放器";
     
-    NSString *localPath = [[NSBundle mainBundle] pathForResource:@"one.mp3" ofType:nil];
-    NSURL *localUrl = [NSURL fileURLWithPath:localPath];
-    YHMusicPlayer *player = [YHMusicPlayer playerWihtLocalMusicUrl:localUrl
-                                           playerScaleChangedBlock:^(CGFloat scale) {
-                                               NSLog(@"进度： %f", scale);
-                                           }
-                                       playerDidFinishPlayingBlock:^{
-                                           NSLog(@"已经播放完毕了");
-                                       }];
-    self.musicPlayer = player;
-    [player play];
+    
+    [self setupFrames];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+- (void)setupFrames
 {
-    NSString *localPath = [[NSBundle mainBundle] pathForResource:@"one.mp3" ofType:nil];
-    NSURL *localUrl = [NSURL fileURLWithPath:localPath];
-    self.musicPlayer.localMusicUrl = localUrl;
+    CGFloat conX = 10;
+    CGFloat conY = 80;
+    CGFloat conW = self.view.frame.size.width - 2 * conX;
+    CGFloat conH = 50;
+    self.controlBackView.frame = CGRectMake(conX, conY, conW, conH);
+    
+    CGFloat palyPauseX = 0;
+    CGFloat palyPauseY = 0;
+    CGFloat palyPauseW = conH;
+    CGFloat palyPauseH = conH;
+    self.playPauseBtn.frame = CGRectMake(palyPauseX, palyPauseY, palyPauseW, palyPauseH);
+    
+    CGFloat stopX = palyPauseX + palyPauseW + 5;
+    CGFloat stopY = palyPauseY;
+    CGFloat stopW = palyPauseW;
+    CGFloat stopH = palyPauseH;
+    self.stopBtn.frame = CGRectMake(stopX, stopY, stopW, stopH);
+    
+    CGFloat slideX = stopX + stopW + 5;
+    CGFloat slideY = 0;
+    CGFloat slideW = conW - slideX - 5;
+    CGFloat slideH = palyPauseH;
+    self.slider.frame = CGRectMake(slideX, slideY, slideW, slideH);
+}
+
+#pragma mark -  延迟加载
+- (YHMusicPlayer *)musicPlayer
+{
+    if (!_musicPlayer) {
+        NSString *localPath = [[NSBundle mainBundle] pathForResource:@"one.mp3" ofType:nil];
+        NSURL *localUrl = [NSURL fileURLWithPath:localPath];
+        _musicPlayer = [YHMusicPlayer playerWihtLocalMusicUrl:localUrl
+                                      playerScaleChangedBlock:^(CGFloat scale) {
+                                          NSLog(@"进度： %f", scale);
+                                          self.slider.value = scale;
+                                      }
+                                  playerDidFinishPlayingBlock:^{
+                                      NSLog(@"已经播放完毕了");
+                                  }];
+    }
+    
+    return _musicPlayer;
+}
+
+/** 播放／暂停按钮 */
+- (UIButton *)playPauseBtn
+{
+    if (!_playPauseBtn) {
+        UIButton *playPauseBtn = [[UIButton alloc] init];
+        [self.controlBackView addSubview:playPauseBtn];
+        _playPauseBtn = playPauseBtn;
+        
+        [playPauseBtn setTitle:@"播放" forState:UIControlStateNormal];
+        [playPauseBtn setTitle:@"暂停" forState:UIControlStateSelected];
+        
+        [playPauseBtn addTarget:self action:@selector(playPauseBtnClicked) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    return _playPauseBtn;
+}
+
+/** 停止按钮 */
+- (UIButton *)stopBtn
+{
+    if (!_stopBtn) {
+        UIButton *stopBtn = [[UIButton alloc] init];
+        [self.controlBackView addSubview:stopBtn];
+        _stopBtn = stopBtn;
+        
+        [stopBtn setTitle:@"停止" forState:UIControlStateNormal];
+        
+        [stopBtn addTarget:self action:@selector(stopBtnClicked) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    return _stopBtn;
+}
+
+/** 进度条 */
+- (UISlider *)slider
+{
+    if (!_slider) {
+        UISlider *slider = [[UISlider alloc] init];
+        [self.controlBackView addSubview:slider];
+        _slider = slider;
+    }
+    
+    return _slider;
+}
+
+/** 播放器控制条背景 */
+- (UIView *)controlBackView
+{
+    if (!_controlBackView) {
+        UIView *controlBackView = [[UIView alloc] init];
+        [self.view addSubview:controlBackView];
+        _controlBackView = controlBackView;
+        
+        controlBackView.backgroundColor = [UIColor grayColor];
+        
+    }
+    return _controlBackView;
+}
+
+#pragma mark - 事件处理
+- (void)playPauseBtnClicked
+{
+    if (!self.playPauseBtn.selected) {
+        [self.musicPlayer play];
+    } else {
+        [self.musicPlayer pause];
+    }
+    
+    self.playPauseBtn.selected = !self.playPauseBtn.selected;
+}
+
+- (void)stopBtnClicked
+{
     [self.musicPlayer stop];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
